cc4b40d93896f1ba95d463aa09fd29579a9076b4
Paulo McNally <paulomcnally@gmail.com>
2019-07-08 21:36:48 -0600

    Modules LOL

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const middleware = require('./middleware');
+const enrouten = require('express-enrouten');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// middlewares
 
+app.use(middleware.auth);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+// routes
+app.use(enrouten({ directory: 'routes' }));
+
+
+// server listen
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
