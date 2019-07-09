b357806b34d9c7c85577ca6e57a07e16c64b3d17
Paulo McNally <paulomcnally@gmail.com>
2019-07-08 21:36:27 -0600

    Add auth middlewarte

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+const config = require('../config/config');
+
+module.exports = function (req, res, next) {
+  if (req.path === '/user/login') {
+    next();
+  } else if (req.headers.authorization === config.auth.token) {
+    next();
+  } else {
+    res.json({
+      error: 'Unautorized',
+      code: 401,
+    });
+  }
+}
